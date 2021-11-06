73f7a01a86f89d8d62c7996015a2c532f7ce4c30
Psyche47 <emmanuel.gomes7181@gmail.com>
2021-11-06 15:32:12 +0600

    Get API added.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const { MongoClient } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -19,7 +20,14 @@ async function run() {
   try {
     await client.connect();
     const database = client.db("travel_planner_eu");
-    const serviceCollection = database.collection("services");
+    const serviceCollection = database.collection("allServices");
+
+    // GET API
+    app.get("/allServices", async (req, res) => {
+      const cursor = serviceCollection.find({});
+      const services = await cursor.toArray();
+      res.send(services);
+    });
   } finally {
     //await client.close();
   }
