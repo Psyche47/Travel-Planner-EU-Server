4aa1ef1cf5309c77f4a01191c51992ec063ba0f1
Psyche47 <emmanuel.gomes7181@gmail.com>
2021-11-15 01:56:09 +0600

    Delete Services ADDED

--- index.js
+++ index.js
@@ -68,6 +68,14 @@ async function run() {
       const result = await bookingCollection.deleteOne(query);
       res.json(result);
     });
+
+    // DELETE SERVICE
+    app.delete("/deleteService/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await serviceCollection.deleteOne(query);
+      res.json(result);
+    });
   } finally {
     //await client.close();
   }
